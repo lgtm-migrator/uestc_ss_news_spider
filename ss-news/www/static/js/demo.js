eba905a88fd8e2d48d02d66b0686d4a35a14033c
suntao <mrls@live.cn>
2016-07-21 21:51:06 +0800

    修改js 边距 预编译模板

--- ss-news/www/static/js/demo.js
+++ ss-news/www/static/js/demo.js
@@ -2,37 +2,32 @@
 var demo = new Framework7({
     cache: true,
     cacheDuration: 1000 * 60,
-    hideNavbarOnPageScroll:true
+    hideNavbarOnPageScroll: true,
+    swipeBackPageActiveArea: 100,
+    precompileTemplates: true
 });
 var $$ = Framework7.$;
 var mainView = demo.addView('.view-main', {
     dynamicNavbar: true,
 });
 
-
-var listTemplate = $$('script#list-template').html();
-var compiledListTemplate = Template7.compile(listTemplate);
-
-//--------------------------------------------
-
-// 加载flag
 var loading = false;
-
-// 每次加载添加多少条目
 var itemsPerLoad = 7;
+var page = 0;
 
-var page = 1;
 
-$$.get(`/home/articles/gets/page/${page}/rows/${itemsPerLoad}`, (data, status) => {
-    loading = false;
-    // 生成新条目的HTML
-    var html = compiledListTemplate({
-        articles: JSON.parse(data)
+var loadmore = () => {
+    page += 1;
+    $$.get(`/home/articles/gets/page/${page}/rows/${itemsPerLoad}`, (data, status) => {
+        loading = false;
+        var html = Template7.templates["list-template"]({
+            articles: JSON.parse(data)
+        });
+        $$('#content-list').append(html);
     });
-    // 添加新条目
-    $$('#content-list').append(html);
-    // 重置加载flag
-});
+}
+
+loadmore();
 
 
 
@@ -41,16 +36,6 @@ $$.get(`/home/articles/gets/page/${page}/rows/${itemsPerLoad}`, (data, status) =
 $$('.infinite-scroll').on('infinite', function() {
     if (loading) return;
     loading = true;
-    page += 1;
-    $$.get(`/home/articles/gets/page/${page}/rows/${itemsPerLoad}`, (data, status) => {
-        // 重置加载flag
-        loading = false;
-        // 生成新条目的HTML
-        var html = compiledListTemplate({
-            articles: JSON.parse(data)
-        });
-        // 添加新条目
-        $$('#content-list').append(html);
-    });
 
+    loadmore();
 });
